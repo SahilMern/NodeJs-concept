af7815fce8d591214286b4dc70035d0e17aee8c3
Sahil yadav <sahil9082811@gmail.com>
2024-12-30 00:47:18 -0600

    add

--- ruf.js
+++ ruf.js
@@ -1,10 +1,2 @@
-// const data = "0x0cc8700006ECA22fc392fEd27D91cb81087dB53E	"
-// console.log(data.toLocaleLowerCase());
-
-
-process.nextTick(() => {
-    console.log('Next tick callback');
-  });
-  
-  console.log('Main thread');
-  
+const memoryUsage = process.memoryUsage();
+console.log(memoryUsage); // Shows memory used by heap, RSS, etc.
