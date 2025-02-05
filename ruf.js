6895bc63273e67a36d87527adf3fe136a28e092f
Sahil yadav <sahil9082811@gmail.com>
2025-02-05 04:40:31 -0600

    jsonwebtoken and straem

--- ruf.js
+++ ruf.js
@@ -1,9 +1,12 @@
-const EventEmitter = require("events")
-const eventemitter = new EventEmitter()
-eventemitter.on("greet", () => {
-  console.log("heyylo");
-  
-})
+const jwt = require("jsonwebtoken")
+const screte_key = "sahilyadavsahilyadavsahilyadavsahilyadav"
+const data = jwt.sign({userId:1,
+  name:"sahil"
+},screte_key, {expiresIn:"1h"})
+console.log(data, "data");
 
-eventemitter.emit("greet")
-eventemitter.emit("hello")
+const verify = jwt.verify("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsIm5hbWUiOiJzYWhpbCIsImlhdCI6MTczODc1MTkxNiwiZXhwIjoxNzM4NzU1NTE2fQ.QyvZNHI5ajp5EhBQU55EvuWkyYjJUfP3BAtZ1iYp-X8", screte_key)
+
+console.log(verify, "verify");
+
+req.headers["a"]
