dc7d2f0ca0be1067c2c3ea879a7c89b283d01615
Sahil yadav <sahil9082811@gmail.com>
2025-02-05 03:53:03 -0600

    event emitter

--- ruf.js
+++ ruf.js
@@ -1,41 +1,9 @@
-// Dummy data (products collection)
-const products = [
-    { name: "Smartwatch", price: 199.99, category: "Electronics", stock: 200 },
-    { name: "Laptop", price: 899.99, category: "Electronics", stock: 50 },
-    { name: "Shirt", price: 29.99, category: "Clothing", stock: 500 },
-    { name: "Jeans", price: 49.99, category: "Clothing", stock: 150 },
-    { name: "Headphones", price: 49.99, category: "Audio", stock: 300 },
-    { name: "Monitor", price: 199.99, category: "Electronics", stock: 100 },
-    { name: "Tablet", price: 299.99, category: "Electronics", stock: 150 }
-  ];
+const EventEmitter = require("events")
+const eventemitter = new EventEmitter()
+eventemitter.on("greet", () => {
+  console.log("heyylo");
   
+})
 
-  const data = async(req, res) => {
-    const limitedProducts = await Product.find({}).limit(3);
-    limitedProducts.forEach(doc => {
-      console.log(doc);
-    });
-    
-  }
-  data()
-
-  // Dummy data (products collection)
-const products = [
-  { name: "Smartwatch", price: 199.99, category: "Electronics", stock: 200 },
-  { name: "Laptop", price: 899.99, category: "Electronics", stock: 50 },
-  { name: "Shirt", price: 29.99, category: "Clothing", stock: 500 },
-  { name: "Jeans", price: 49.99, category: "Clothing", stock: 150 },
-  { name: "Headphones", price: 49.99, category: "Audio", stock: 300 },
-  { name: "Monitor", price: 199.99, category: "Electronics", stock: 100 },
-  { name: "Tablet", price: 299.99, category: "Electronics", stock: 150 }
-];
-
-
-const data = async(req, res) => {
-  const limitedProducts = await Product.find({}).limit(3);
-  limitedProducts.forEach(doc => {
-    console.log(doc);
-  });
-  
-}
-data()
+eventemitter.emit("greet")
+eventemitter.emit("hello")
