95a6355ca7fd0ab1d08e0bb62e39a75d2f3ae7bf
Sahil yadav <sahil9082811@gmail.com>
2025-02-18 01:50:38 -0600

    node js pull data

--- ruf.js
+++ ruf.js
@@ -1,28 +1,7 @@
-const mongoose = require("mongoose");
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-});
-const User = mongoose.model("User", userSchema);
-module.exports = User;
+let data = 1
+if (true) {
+  let data = 10; // Block-scoped, so different from the outer y
+  console.log(data); // 10
+}
 
-// ---------------------------------------------------------------
-
-
-const postSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-});
-const Post = mongoose.model("Post", postSchema);
-module.exports = Post;
-
-
-app.get("/posts", async (req, res) => {
-  try {
-    const posts = await Post.find().populate("user", "name email"); // Populate user field with name and email
-    res.json(posts);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+console.log(data);
