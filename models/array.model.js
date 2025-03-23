eecd668555a74a10ad0a812966925d23b244dff7
SahilMern <sahil9082811@gmail.com>
2025-03-23 23:40:37 +0530

    builing useraddress schema design

--- models/array.model.js
+++ models/array.model.js
@@ -1,15 +1,62 @@
+// const mongoose = require("mongoose");
+// const arrayDocumentSchema = mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true,
+//   },
+//   arrayField: [
+//     {
+//       type: String,
+//     },
+//   ],
+// });
+// const ArrayField =
+//   mongoose.model.arrayField ||
+//   mongoose.model("arrayfield", arrayDocumentSchema);
+// module.exports = ArrayField;
+
 const mongoose = require("mongoose");
-const arrayDocumentSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
   },
-  arrayField: [
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  address: [
     {
-      type: String,
+      type: {
+        type: String,
+        enum: ["home", "office"], // Yeh define karta hai ki address type sirf home ya office ho sakta hai
+        required: true,
+      },
+      address_line: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      country: {
+        type: String,
+        required: true,
+      },
+      postal_code: {
+        type: String,
+        required: true,
+      },
     },
   ],
 });
-const ArrayField = mongoose.model.arrayField || mongoose.model("arrayfield", arrayDocumentSchema)
 
-module.exports = ArrayField;
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
